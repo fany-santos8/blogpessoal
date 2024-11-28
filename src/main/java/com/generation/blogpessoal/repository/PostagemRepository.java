91d63de9841d30a99ed70faf61d3b6392874ff31
fany-santos8 <estefany_fanysantos@hotmail.com>
2024-11-28 12:33:56 -0300

    task2

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	public Optional<Postagem> findByTitulo(String titulo);
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
