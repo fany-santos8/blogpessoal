400d2ac17f8f8ef1bf8530a8001d95f655ebb33b
fany-santos8 <estefany_fanysantos@hotmail.com>
2024-11-29 15:50:45 -0300

    task3

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,6 +31,9 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 
 	@GetMapping // Buscar a lista de postagens
 	public ResponseEntity<List<Postagem>> getAll() {
@@ -51,8 +55,15 @@ public class PostagemController {
 
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.status(HttpStatus.CREATED)
+		if (postagemRepository.existsById(postagem.getId())) {
+
+		if (temaRepository.existsById(postagem.getTema().getId())) 
+		return ResponseEntity.status(HttpStatus.OK)
 				.body(postagemRepository.save(postagem));
+		
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+	}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@PutMapping
