f1aa61fe8ac15be8f440bda6147e6ddf5a1d59fe
fany-santos8 <estefany_fanysantos@hotmail.com>
2024-11-29 20:49:55 -0300

    task3

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -55,23 +55,28 @@ public class PostagemController {
 
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-		if (postagemRepository.existsById(postagem.getId())) {
-
-		if (temaRepository.existsById(postagem.getTema().getId())) 
-		return ResponseEntity.status(HttpStatus.OK)
+		if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
 		
 		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
-	}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	
 	}
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-				.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		if (postagemRepository.existsById(postagem.getId())) {
+
+		 if (postagemRepository.existsById(postagem.getTema().getId()))
+		      return ResponseEntity.status(HttpStatus.OK)
+					 .body(postagemRepository.save(postagem));
+		 
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+		
 	}
 	@ResponseStatus (HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
